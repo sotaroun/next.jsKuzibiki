ef9dd6829eb0697bee4a9af099885bcb4dccc5ef
sotaroun <acesoichi@yahoo.co.jp>
2025-02-26 08:07:44 +0900

    first

---
+++ src/app/hoge/page.tsx
@@ -0,0 +1,9 @@
+export default function Home () {
+    return (
+        <div>
+            <p>Hello, Next.js</p>
+        </div>
+    );
+}
+
+console.log("error");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/hoge"
           target="_blank"
           rel="noopener noreferrer"
         >
